1e20104a5766899679ee9eaff9104718efb5f09a
carlost-MSFT <carlost@microsoft.com>
2023-02-23 21:44:16 -0800

    Add files via upload

---
+++ SLBProbeDownScenario003.ps1
@@ -0,0 +1,11 @@
+﻿Add-WindowsFeature Web-Server;
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
+New-Item -ItemType Directory c:\inetpub\wwwroot\probe
+Add-Content -Path "C:\inetpub\wwwroot\probe\Default.htm" -Value $($env:computername),PROBE
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-command " $rule = Get-NetFirewallRule -Direction Inbound -DisplayGroup \"world*\";if(($rule).action -eq \"Allow\"){Set-NetFirewallRule -Direction Inbound -Action Block -DisplayGroup \"world*\"} else {Set-NetFirewallRule -Direction Inbound -DisplayGroup \"World*\" -Action Allow}"'
+$mins = 2,3,4,5,6 | Get-Random
+$trigger =  New-ScheduledTaskTrigger -Daily -At ((get-date).AddMinutes($mins))
+$task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Port80Toggler" -Description "Port80Toggler" -User "NT Authority\System"
+$task.Triggers.Repetition.Duration = "P5D"
+$task.Triggers.Repetition.Interval = "PT06M"
+$task | Set-ScheduledTask
